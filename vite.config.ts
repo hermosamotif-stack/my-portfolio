9373e73bc5baf9419dbffca06e2b14200641a1d7
hermosamotif-stack <hermosamotif@gmail.com>
2026-02-18 19:34:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/my-portfolio/',  // <--- ഈ വരിയാണ് പ്രധാനം. ഇത് മിസ്സ് ആക്കരുത്!
       server: {
         port: 3000,
         host: '0.0.0.0',
